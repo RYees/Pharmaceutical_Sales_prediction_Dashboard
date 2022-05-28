f8f270fad1a4bea828edb654f582cd6ac5ae1512
Rehmet Yeshanew <ryfgmz87@gmail.com>
2022-05-28 22:14:06 +0300

    commits changes

--- pages/Pharmaceutical_Sales.py
+++ pages/Pharmaceutical_Sales.py
@@ -1,18 +1,26 @@
-from distutils.command.upload import upload
-from shutil import chown
-from itsdangerous import exc
 import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
+import pickle
+import seaborn as sns
+# with open('rf_model.plk', 'wb') as f:
+#   pickle.dump(rf, f)
+# with open('rf_model.plk', 'rb') as f:
+#     mf = pickle.load(f)
+import time
+from PIL import Image
 
 def app():
     # st.write("Pharmaceutical Sales Prediction")
-
+ 
     st.markdown("""<span style="margin-top:100px;"></span>""", unsafe_allow_html=True)  
 
     uploaded_file = st.sidebar.file_uploader(label="Upload your CSV or Excel file with the following 'Date', 'IsHoliday','IsWeekend','IsPromo' column names.", type=['csv', 'xlsx'])
+    
+    # lstm_model = open("..model/istm_model.pkl","rb")
+    # lstm_model = pickle.load(lstm_model)
     
     global df
     if uploaded_file is not None:
@@ -24,6 +32,9 @@ def app():
             df = pd.read_excel(uploaded_file)
     try:
         st.write(df)
+        fig = plt.figure(figsize=(10, 4))
+        sns.lineplot(x = "Month", y = "Sales", data = df)
+        st.pyplot(fig)
     except Exception as e:
         print(e)
         st.write("Please upload file to the application")
